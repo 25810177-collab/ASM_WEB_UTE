41c8827c56c4ccbfe4e3ab7f060f311e16b42b1d
dutv <tranvandu19971029@gmail.com>
2026-09-09 20:22:36 +0700

    admin: Cap nhat cac Enum AssignmentStatus, CommitteeRole, CouncilStatus

--- src/main/java/ute/edu/enums/AssignmentStatus.java
+++ src/main/java/ute/edu/enums/AssignmentStatus.java
@@ -1,5 +1,9 @@
 package ute.edu.enums;
 
+/** Trạng thái của việc phân công đề tài vào hội đồng. */
 public enum AssignmentStatus {
-    ASSIGNED, EVALUATED
+    // Đã phân công nhưng hội đồng chưa chấm đủ điểm.
+    ASSIGNED,
+    // Hội đồng đã hoàn tất việc chấm điểm.
+    EVALUATED
 }
